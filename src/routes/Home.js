e893791f0b21fe697d24d73e75aa3da942d71ef2
attack-titan4 <nayannikam4@gmail.com>
2024-12-16 14:57:12 +0530

    rrrrr

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import HeroImage from '../components/HeroImage'
 import Footer from '../components/Footer'
-import Work from './components/Work'
+import Work from '../components/Work'
 
 
 const Home = () => {
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import HeroImg2 from '../components/HeroImg2'
-import Work from './components/Work.js'
+import Work from '../components/Work.js'
 
 const Project = () => {
   return (
